16ae1326f3964ef1a6455e23138a5e3d691d2a55
Barbara Candido <barbara.candido@unifesp.br>
2020-11-06 13:01:04 -0300

    Exercício Blog Pessoal com métodos DELETE, POST e PUT

---
+++ exercicio_dia_3/blog_barbara/src/main/java/com/barbara/blog_barbara/controller/PostagemController.java
@@ -0,0 +1,73 @@
+package com.barbara.blog_barbara.controller;
+
+import java.util.List;
+
+import com.barbara.blog_barbara.model.Postagem;
+import com.barbara.blog_barbara.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagem") //indica que é um ponto de acesso
+@CrossOrigin("*")
+
+public class PostagemController {
+	
+	@Autowired //para que o spring instancie a interface do repositório
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo (@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem>  post (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	
+	@PutMapping
+	public ResponseEntity<Postagem>  put (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ exercicio_dia_3/blog_barbara/src/test/java/com/barbara/blog_barbara/BlogBarbaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbara.blog_barbara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBarbaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
