697b95830ebebadbfb5cbc3d3b518afd9d3fa47f
Barbara Candido <barbara.candido@unifesp.br>
2020-11-10 22:41:39 -0300

    Back-end de uma loja de games versão 1.0

---
+++ lojaGame/src/main/java/com/generation/lojaGame/model/Produto.java
@@ -0,0 +1,91 @@
+package com.generation.lojaGame.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	
+	@Size(min = 1, max = 200)
+	private String nome;
+	
+	@Column
+	private boolean estoque;
+	
+	@Column
+	private int quantidade;
+	
+	@Column
+	private double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(boolean estoque) {
+		this.estoque = estoque;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+
+}
