0b4f8123bef7c0f336ec920d0adb4f88753cf194
Barbara Candido <barbara.candido@unifesp.br>
2020-11-13 23:28:33 -0300

    Adição das camadas de segurança

---
+++ blog_barbara/src/main/java/com/barbara/blog_barbara/BlogBarbaraApplication.java
@@ -0,0 +1,13 @@
+package com.barbara.blog_barbara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBarbaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBarbaraApplication.class, args);
+	}
+
+}
